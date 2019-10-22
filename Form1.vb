18f2d88f48780177421346935e5ea955081f6454
Lauriita <laurahb123@gmail.com>
2019-10-22 16:02:30 +0200

    Agregar archivos de proyecto.

---
+++ Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+
+    Dim entrada As Integer
+    Dim suma As Integer
+    Dim total As Integer
+    Dim media As Decimal
+    Dim menor As Integer
+    Dim mayor As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        entrada = InputBox("introduce 1 numero")
+        MsgBox(10, "maximo")
+        menor = entrada
+        mayor = entrada
+
+        For operaciones As Integer = 0 To 10
+
+            suma = suma + entrada
+            total = total + 1
+            menor = If(menor < entrada) Then  menor = entrada End If
+            mayor = If(menor > entrada) Then  mayor = entrada End If
+
+        Next
+
+        media = suma / total
+
+        TextBox3.Text = media
+        TextBox1.Text = menor
+        TextBox2.Text = mayor
+
+
+
+    End Sub
+End Class
